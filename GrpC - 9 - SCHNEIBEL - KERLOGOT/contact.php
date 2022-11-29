5d6334e17efb7a1f8fc4ac6cb66ba35ceadb1c3d
Samantha <sschneibel@gaming.tech>
2022-11-29 19:08:47 +0100

    contact

--- GrpC - 9 - SCHNEIBEL - KERLOGOT/contact.php
+++ GrpC - 9 - SCHNEIBEL - KERLOGOT/contact.php
@@ -1,6 +1,17 @@
 <?php require_once "nav.php" ?>
 
     <h1 class="center wow animate__animated animate__fadeInDown">Pour nous contacter :</h1>
-    <a<h3 class="center wow animate__animated animate__fadeInDown"></h3>
+    
+    <div id="contact-us" class="container">
+        <form method="post" action="action/contact.php">
+        <p>Qui nous envoie ce mail ?</p>
+        <input type="text" mail="mail" placeholder="exemple@mail.com">
+        <p>Quel est l'objet de votre mail ?</p>
+        <input type="text" object="object" placeholder="Problème">
+        <p>Comment pouvons nous vous aider ?</p>
+        <input type="text" contenu="contenu" placeholder="Bonjour, j'ai tel problème...">
+        <input type="submit" value="Envoyer">
+        </form>
+    </div>
     
 <?php require_once "footer.php" ?>
