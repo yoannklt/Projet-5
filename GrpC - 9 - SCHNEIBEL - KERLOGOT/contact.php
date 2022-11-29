ef3def85a2a219ade4515354b25896a9a3d9d2e0
Samantha <sschneibel@gaming.tech>
2022-11-29 17:56:34 +0100

    gigachad antoine <3

--- GrpC - 9 - SCHNEIBEL - KERLOGOT/contact.php
+++ GrpC - 9 - SCHNEIBEL - KERLOGOT/contact.php
@@ -1,2 +1,6 @@
 <?php require_once "nav.php" ?>
+
+    <h1 class="center wow animate__animated animate__fadeInDown">Pour nous contacter :</h1>
+    <a<h3 class="center wow animate__animated animate__fadeInDown"></h3>
+    
 <?php require_once "footer.php" ?>
