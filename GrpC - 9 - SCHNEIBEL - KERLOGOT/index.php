a86e0d4801d5fedf860da264c865d23ee544cff5
yoannklt <ykerlogot@gaming.tech>
2022-11-29 19:10:54 +0100

    contact fixed

--- GrpC - 9 - SCHNEIBEL - KERLOGOT/index.php
+++ GrpC - 9 - SCHNEIBEL - KERLOGOT/index.php
@@ -131,7 +131,7 @@
               <div class="col l6 s12">
                 <h5 class="green-text text-lighten-5">Retrouvez-nous</h5>
                 <p class="green-text text-lighten-5">Sur Instagram et Twitter mais aussi via nos adresses mail : sschneibel@gaming.tech et ykerlogot@gaming.tech</p>
-                <li><a href=".php" class="green-text text-lighten-5">Nous contacter</a></li>
+                <li><a href="contact.php" class="green-text text-lighten-5">Nous contacter</a></li>
                 <p class="green-text text-lighten-5">Made with <i id="heart" class="material-icons red-text tiny">favorite</i>&nbsp;in Lyon</p>
               </div>
               <div class="col l4 offset-l2 s12">
