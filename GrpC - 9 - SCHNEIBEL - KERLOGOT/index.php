1e7b7adf19078077bca0fc9535350dd98c1301a1
Samantha <sschneibel@gaming.tech>
2022-11-30 08:41:04 +0100

    zaertyu

--- GrpC - 9 - SCHNEIBEL - KERLOGOT/index.php
+++ GrpC - 9 - SCHNEIBEL - KERLOGOT/index.php
@@ -1,3 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.css">
+    <link type="text/css" rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+    <!--Let browser know website is optimized for mobile-->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="img/logo-site.png">
+    <title>Sam&Yoann</title>
+</head>
+
+<body>
 <?php require_once "nav.php" ?>
     
     <h1 class="center green-text text-lighten-2 wow animate__animated animate__fadeInDown">Samantha & Yoann</h1>
