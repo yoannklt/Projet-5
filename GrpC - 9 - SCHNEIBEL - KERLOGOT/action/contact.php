c15e8594e3bc16397e1e7aab486873689362586a
Samantha <sschneibel@gaming.tech>
2022-11-30 09:53:38 +0100

    jhg

--- GrpC - 9 - SCHNEIBEL - KERLOGOT/action/contact.php
+++ GrpC - 9 - SCHNEIBEL - KERLOGOT/action/contact.php
@@ -1,12 +1,33 @@
-<?php
-    $email = "exemple@mail.com";
-    $objet = "Objet du mail";
-    $content = "<b>Mon message<b><br />Voici le contenu de mon message";
-    $headers = array('MIME-Version: 1.0','Content-type: text/html; charset=utf8');
-    
-    if(mail($email,$objet,$content,$headers)){
-        //le mail est bien parti ! Pas d'erreur
-    }else{
-        //on a une erreur quelque part
-    }
+<?php 
+require_once "../cfg/config.php"; 
+if(empty($_POST['user_name'])){
+     $_SESSION['error']= "No Username";
+     header('Location: ../index.php');
+     exit();
+}
+if (empty($_POST['password'])){
+     $_SESSION['error']= "No Password";
+     header('Location: ../index.php');
+     exit();
+}
+
+//vérifie si l'utilisateur est connecté ou non
+if(isset($_SESSION['user_name'])){
+     echo "Bonjour ".$_SESSION['user']['first_name'];
+ }else{
+     echo "Vous n'êtes pas connecté";
+ }
+
+$sql = "SELECT * FROM users WHERE user_name='".$_POST['user_name']."' AND password='".$_POST['password']."'"; 
+$pre = $pdo->prepare($sql); 
+$pre->execute();
+$user = $pre->fetch(PDO::FETCH_ASSOC);
+if(empty($user)){ //vérifie si le resultat est vide !
+     //non connecté
+     echo "Utilisateur ou mot de passe incorrect !";
+}else{
+     $_SESSION['user'] = $user; //on enregistre que l'utilisateur est connecté
+}
+
+header('Location:../index.php');//on le redirige sur la page d'accueil du site !
 ?>
