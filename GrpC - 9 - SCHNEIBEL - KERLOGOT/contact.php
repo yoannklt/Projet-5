22804d6c157fa8ad4ef4a9909e56ff9d919a8931
Samantha <sschneibel@gaming.tech>
2022-11-30 09:49:41 +0100

    sql update

--- GrpC - 9 - SCHNEIBEL - KERLOGOT/contact.php
+++ GrpC - 9 - SCHNEIBEL - KERLOGOT/contact.php
@@ -30,9 +30,9 @@
         <p>Qui nous envoie ce mail ?</p>
         <input type="text" mail="mail" placeholder="exemple@mail.com">
         <p>Quel est l'objet de votre mail ?</p>
-        <input type="text" object="object" placeholder="Problème">
+        <input type="text" objet="objet" placeholder="Problème">
         <p>Comment pouvons nous vous aider ?</p>
-        <input type="text" contenu="contenu" placeholder="Bonjour, j'ai tel problème...">
+        <input type="text" content="content" placeholder="Bonjour, j'ai tel problème...">
         <input type="submit" value="Envoyer">
         </form>
     </div>
