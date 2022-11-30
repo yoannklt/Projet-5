5d52900a59bbbf69f30363edc4fd793eb672f7d5
yoannklt <ykerlogot@gaming.tech>
2022-11-30 08:38:17 +0100

    connexion

--- GrpC - 9 - SCHNEIBEL - KERLOGOT/index.php
+++ GrpC - 9 - SCHNEIBEL - KERLOGOT/index.php
@@ -56,6 +56,13 @@
     <h1 class="center green-text text-lighten-2 wow animate__animated animate__fadeInDown">Samantha & Yoann</h1>
     <h2 class="center our_projects wow animate__animated animate__fadeInDown">Nos projets</h2>
 
+    <?php    if(isset($_SESSION['user'])){
+            echo "Bonjour ".$_SESSION['user']['first_name'];
+        }else{
+            echo "Vous n'êtes pas connecté";
+        }
+    ?>
+    
     <div id="projects" class="container wow animate__animated animate__fadeIn">
         <div class="first carousel carousel-slider center" style="height: 500px;">
             <div class="first-project carousel-item">
